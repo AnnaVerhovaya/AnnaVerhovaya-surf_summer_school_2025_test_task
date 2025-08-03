588969a0fa2877d0ae716f3c129c84a5f70b873c
/erAnn <99863500+AnnaVerhovaya@users.noreply.github.com>
2025-08-03 14:20:21 +0300

    Initial commit

--- lib/core/domain/interfaces/i_search_history_client.dart
+++ lib/core/domain/interfaces/i_search_history_client.dart
@@ -1,14 +1,7 @@
 /// Интерфейс для работы с историей поиска
 abstract interface class ISearchHistoryClient {
-  /// Сохранить запрос в историю
   Future<void> saveSearchQuery(String query);
-
-  /// Получить историю поиска
   Future<List<String>> getSearchHistory();
-
-  /// Удалить конкретный запрос из истории
   Future<void> removeSearchQuery(String query);
-
-  /// Очистить всю историю поиска
   Future<void> clearSearchHistory();
 }
